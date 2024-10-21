7f887847abc5d6c4e83f06ce8a1cbb37957509c3
Dimos Tsouros <dimosts988@gmail.com>
2024-10-21 09:22:25 +0200

    findscope (no bias) test

--- tests/test_findscope.py
+++ tests/test_findscope.py
@@ -12,22 +12,26 @@ class TestFindScope:
 
     def test_findscope(self):
         a, b, c, d = cp.intvar(0, 9, shape=4)  # variables
+        vars_array = cp.cpm_array([a, b, c, d])
 
         oracle_model = cp.Model()
-        oracle_model += cp.AllDifferent([a, b, c, d]).decompose()
+        oracle_model += cp.AllDifferent(vars_array).decompose()
         oracle_model += 10 * c + d == 3 * (10 * a + b)
         oracle_model += 10 * d + a == 2 * (10 * b + c)
         constraints = toplevel_list(oracle_model.constraints)
 
-        instance = ca.ProblemInstance(variables=cp.cpm_array([a, b, c, d]))
+        instance = ca.ProblemInstance(variables=cp.cpm_array(vars_array))
         ca_env = ca.ActiveCAEnv(find_scope=ca.FindScope())
 
-        for c in range(len(constraints)):
-            model = cp.Model(constraints[:c] + constraints[c+1:]) # all constraints except this
-            model += ~constraints[c]
-            model.solve()
+        for con in range(len(constraints)):
+            model = cp.Model(constraints[:con] + constraints[con + 1:])  # all constraints except this
+            model += ~constraints[con]
+
+            if not model.solve():
+                continue
 
             ca_env.init_state(oracle=ca.ConstraintOracle(oracle_model.constraints), instance=instance, verbose=1)
-            Y = ca_env.run_find_scope([a, b, c, d])
+            Y = ca_env.run_find_scope(vars_array)
 
-            assert Y == get_variables(constraints[c])
+            assert ca.utils.compare_scopes(Y,
+                                           get_variables(constraints[con])), f"{Y}, {get_variables(constraints[con])}"
